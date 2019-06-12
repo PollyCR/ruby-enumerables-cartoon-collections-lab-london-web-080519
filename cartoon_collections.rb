c295ada7961e74dd9eb8c1f1688eb7fdc20292e8
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-12 20:45:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,8 @@ dwarves.collect.each_with_index do |dwarf,index|
 end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+calls.collect.
 end
 
 def long_planeteer_calls# code an argument here
